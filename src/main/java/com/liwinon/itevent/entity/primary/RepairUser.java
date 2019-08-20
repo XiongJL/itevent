41a8f49d6ed78e5d0710f4b2322aaecf7e283ebe
1907128000 <1907128000@HIT-I00740-N.LIWINON.COM>
2019-08-20 16:23:12 +0800

    维护人员

---
+++ src/main/java/com/liwinon/itevent/entity/primary/RepairUser.java
@@ -0,0 +1,56 @@
+package com.liwinon.itevent.entity.primary;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "ITE_RepairUser")
+public class RepairUser {
+	 	@Id
+	    private String userid;  //工号
+	    private String wxid;	//企业微信账号
+	    private String name;	//姓名
+	    private String userlevel;//级别
+	    private String phone;    //电话
+		public String getUserid() {
+			return userid;
+		}
+		public void setUserid(String userid) {
+			this.userid = userid;
+		}
+		public String getWxid() {
+			return wxid;
+		}
+		public void setWxid(String wxid) {
+			this.wxid = wxid;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public String getUserlevel() {
+			return userlevel;
+		}
+		public void setUserlevel(String userlevel) {
+			this.userlevel = userlevel;
+		}
+		public String getPhone() {
+			return phone;
+		}
+		public void setPhone(String phone) {
+			this.phone = phone;
+		}
+		@Override
+		public String toString() {
+			return "RepairUser [userid=" + userid + ", wxid=" + wxid + ", name=" + name + ", userlevel=" + userlevel
+					+ ", phone=" + phone + "]";
+		}
+	    
+}

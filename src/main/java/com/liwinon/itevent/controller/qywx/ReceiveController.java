cd112b77e28bd43cdabe2b5d8cbf27f92a79f3fc
XiongJL <511593318@qq.com>
2019-08-27 10:47:12 +0800

    微信接收口必须在5秒内回执,不然会重连3次!

--- src/main/java/com/liwinon/itevent/controller/qywx/ReceiveController.java
+++ src/main/java/com/liwinon/itevent/controller/qywx/ReceiveController.java
@@ -1,5 +1,6 @@
 package com.liwinon.itevent.controller.qywx;
 
+import com.liwinon.itevent.annotation.PasssToken;
 import com.liwinon.itevent.qywx.WxConfig;
 import com.liwinon.itevent.service.ReceiveService;
 import com.liwinon.itevent.qywx.WxApi;
@@ -17,6 +18,8 @@ import javax.servlet.http.HttpServletResponse;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.StringReader;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 
 @Controller
 @RequestMapping("/itevent")
@@ -30,33 +33,33 @@ public class ReceiveController {
      * 验证接口
      * @return
      */
-//    @GetMapping("/receive")
-//    @ResponseBody
-//    @PasssToken
-//    public String receiveForCheck(String msg_signature, String timestamp, String nonce, String echostr){
-//        String msg = "";
-//		String time = "";
-//		String non = "";
-//		String echo = "";
-//		try {
-//			msg = URLDecoder.decode(msg_signature, "utf-8");
-//			time = URLDecoder.decode(timestamp, "utf-8");
-//			non = URLDecoder.decode(nonce, "utf-8");
-//			echo = URLDecoder.decode(echostr, "utf-8");
-//			System.out.println("msg_signature:" + msg);
-//			System.out.println("timestamp" + time);
-//			System.out.println("nonce" + non);
-//			System.out.println("echostr" + echo);
-//			WXBizMsgCrypt wxcpt = new WXBizMsgCrypt(Token, EncodingAESKey, Corpid);
-//			String result = wxcpt.VerifyURL(msg, time, non, echo);
-//			// update用户
-//			return result;
-//		} catch (Exception e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//		return "测试中";
-//    }
+    @GetMapping("/receive")
+    @ResponseBody
+    @PasssToken
+    public String receiveForCheck(String msg_signature, String timestamp, String nonce, String echostr){
+        String msg = "";
+		String time = "";
+		String non = "";
+		String echo = "";
+		try {
+			msg = URLDecoder.decode(msg_signature, "utf-8");
+			time = URLDecoder.decode(timestamp, "utf-8");
+			non = URLDecoder.decode(nonce, "utf-8");
+			echo = URLDecoder.decode(echostr, "utf-8");
+			System.out.println("msg_signature:" + msg);
+			System.out.println("timestamp" + time);
+			System.out.println("nonce" + non);
+			System.out.println("echostr" + echo);
+			WXBizMsgCrypt wxcpt = new WXBizMsgCrypt(WxConfig.Token.getValue(), WxConfig.EncodingAESKey.getValue(), WxConfig.Corpid.getValue());
+			String result = wxcpt.VerifyURL(msg, time, non, echo);
+			// update用户
+			return result;
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return "测试中";
+    }
 
     /**
      * 接收APP消息
@@ -64,7 +67,8 @@ public class ReceiveController {
      */
     @PostMapping("/receive")
     @ResponseBody
-    public String receive(String msg_signature, String timestamp, String nonce, @RequestBody String body, HttpServletResponse response){
+    @PasssToken
+    public String receive(String msg_signature, String timestamp, String nonce, @RequestBody String body, HttpServletResponse response) throws UnsupportedEncodingException, AesException {
         WXBizMsgCrypt wxcpt = null;
 		try {
 			wxcpt = new WXBizMsgCrypt(WxConfig.Token.getValue(), WxConfig.EncodingAESKey.getValue(), WxConfig.Corpid.getValue());
@@ -74,11 +78,12 @@ public class ReceiveController {
 		System.out.println("sReqMsgSig: "+msg_signature);
 		System.out.println("sReqTimeStamp: "+timestamp);
 		System.out.println("sReqNonce: "+nonce);
+        String echo = URLDecoder.decode(body, "utf-8");
         GetMSG msg = new GetMSG(msg_signature,timestamp,nonce,body,wxcpt);
         if (WxConfig.Corpid.getValue().equals(msg.getToUserName())){
             receiveService.GetCorp(msg);
         }
         response.setStatus(200);
-        return "200";
+        return wxcpt.VerifyURL(msg_signature, timestamp, nonce,echo);
     }
 }
